1f66bb91520d0e0132a8d456e00af0b1b15eb6c6
Willow Behar <bbrandon.behar@protonmail.com>
2020-10-16 09:44:37 -0500

    Added start to Caesar Cipher coding challenge

---
+++ Willow_Cypher_Coding_Challenge.py
@@ -0,0 +1,13 @@
+import sys
+
+if __name__ == "__main__":
+    main()
+
+def main():
+    if "-E" in sys.argv:
+        encrypt()
+    if "-D" in sys.argv:
+        //Decrypt
+    if "-h" in sys.argv:
+        #Help
+        
