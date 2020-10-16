67b0c1114667f6c7d3f986c5828d96f62ae8ed01
Willow Behar <bbrandon.behar@protonmail.com>
2020-10-16 11:08:18 -0500

    Added Flag checking, Encryption and decryption

--- Willow_Cypher_Coding_Challenge.py
+++ Willow_Cypher_Coding_Challenge.py
@@ -10,11 +10,41 @@ if __name__ == "__main__":
     main()
 
 def main():
+    #Get Key
+    if "-k" in sys.argv:
+        key_flag_location = sys.argv.index("-k")
+        key = sys.argv[key_flag_location + 1]
     if "-E" in sys.argv:
+        #Get String
+        encrypt_flag_location = sys.argv.index("-E")
+        encrypt_string = sys.argv[encrypt_flag_location + 1]
         #Encrypt
-        encrypt()
+        Encrypt(key, encrypt_string)
     if "-D" in sys.argv:
+        #Get String
+        decrypt_flag_location = sys.argv.index("-D")
+        decrypt_string = sys.argv[decrypt_flag_location + 1]
         #Decrypt
+        Decrypt(key, decrypt_string)
     if "-h" in sys.argv:
         #Help
         
+def Encrypt(key, phrase):
+    new_string = ""
+    if phrase is not "":
+        for letter in phrase:
+            letter_value = ord(letter)
+            if 94 <= letter_value => 122:
+                letter_value += key
+                if letter_value > 122:
+                    while letter_value > 122:
+                        offset = letter_value - 122
+                        letter_value = 94 + offset
+                if letter_value < 94:
+                    while letter_value < 94:
+                        offset = letter_value - 94
+                        letter_value = 122 + offset
+             new_string.join()
+
+def Decrypt(key, phrase):
+    if phrase is not "":
