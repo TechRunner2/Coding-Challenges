ac27e95864e9e44485739d318c70757c888fa7ef
Willow Behar <bbrandon.behar@protonmail.com>
2020-10-16 09:47:30 -0500

    Added Author, Date, Description

--- Willow_Cypher_Coding_Challenge.py
+++ Willow_Cypher_Coding_Challenge.py
@@ -1,3 +1,9 @@
+"""
+Author: Willow Behar
+Date Created: 10/16/20
+Description: A Caesar Cipher coding challenge given by the Crunix Club"
+"""
+
 import sys
 
 if __name__ == "__main__":
@@ -5,9 +11,10 @@ if __name__ == "__main__":
 
 def main():
     if "-E" in sys.argv:
+        #Encrypt
         encrypt()
     if "-D" in sys.argv:
-        //Decrypt
+        #Decrypt
     if "-h" in sys.argv:
         #Help
         
